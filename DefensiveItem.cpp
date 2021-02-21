18e0da8e087ad4670e4b7cea0fc555007165849a
Jamie Webber <jamiecwebber@gmail.com>
2021-02-21 01:56:35 +0000

    made all changes from pull request except character.cpp

--- DefensiveItem.cpp
+++ DefensiveItem.cpp
@@ -1,14 +1,9 @@
 #include "DefensiveItem.h"
-// #include "Dwarf.h"
-// #include "Paladin.h"
-// #include "DragonSlayer.h"
-// #include "Dragon.h"
+
 #include "Utility.h"
 
 void DefensiveItem::use(Character* character)
 {
     useDefensiveItem(character, *this);
-
-    //make your defensive item use the appropriate Utility helper functions
 }
 
--- DragonSlayer.cpp
+++ DragonSlayer.cpp
@@ -4,7 +4,6 @@
 #include "Utility.h"
 
 //DragonSlayer::DragonSlayer
-
 DragonSlayer::DragonSlayer(std::string name_, int hp_, int armor_) : Character(hp_, armor_, 4), name(name_) 
 {
     attackItem = std::unique_ptr<Item>(new AttackItem());
@@ -23,18 +22,17 @@ void DragonSlayer::attack(Character& other)
     std::cout << name << " is attacking " << other.getName() << " !!" << std::endl;
     if( auto* dragon = dynamic_cast<Dragon*>(&other) )
     {
-        // assert(false);
+
         //DragonSlayers get a 10x boost when attacking dragons, from their attack item.
         //so they should USE their attack item before attacking the dragon... 
         //note that items are single-use only, so you need to reset it after use.  
         //look in the Character class for how the other item types are reset after use.
-        attackItem.get()->use(this);
-        attackItem.reset();
-        // void boostAttackDamage( int amount )
-        // {
-        //     attackDamage += amount;
-        //     std::cout << getName() << "'s attack damage level has been boosted to " << attackDamage << std::endl;
-        // }
+        if ( attackItem )
+        {
+            attackItem->use(this);
+            attackItem.reset();
+        }
+
         while( dragon->getHP() > 0 )
         {
           dragon->takeDamage(attackDamage);
